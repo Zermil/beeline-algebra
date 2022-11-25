04902c7c6d01db0e248995a1417887339247fce5
Aiden <marcin.madry@interia.com>
2022-11-25 18:55:03 +0100

    useful preprocessor directives, fixed indentation

--- bee_algebra.h
+++ bee_algebra.h
@@ -1,5 +1,5 @@
-#ifndef BEELINE_ALGEBRA_H_
-#define BEELINE_ALGEBRA_H_
+#ifndef BEELINE_ALGEBRA_H
+#define BEELINE_ALGEBRA_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -11,7 +11,7 @@
 #define GM_PRINT_VEC2(vec2_ptr) _gm_print_vec2(vec2_ptr)
 #define GM_PRINT_VEC3(vec3_ptr) _gm_print_vec3(vec3_ptr)
 #define GM_PRINT_VEC4(vec4_ptr) _gm_print_vec4(vec4_ptr)
-#else
+#else // DEBUG
 #define GM_ASSERT(cond, msg) ((void)0)
 #define GM_PRINT_MAT4(mat4_ptr) ((void)0)
 #define GM_PRINT_VEC2(vec2_ptr) ((void)0)
@@ -20,88 +20,88 @@
 #endif // NDEBUG
 
 union Vec2 {
-  float elements[2];
+    float elements[2];
   
-  struct {
-    float x;
-    float y;
-  };
+    struct {
+        float x;
+        float y;
+    };
 };
 
 union Vec3 {
-  float elements[3];
+    float elements[3];
   
-  struct {
-    float x;
-    float y;
-    float z;
-  };
+    struct {
+        float x;
+        float y;
+        float z;
+    };
   
-  struct {
-    float r;
-    float g;
-    float b;
-  };
+    struct {
+        float r;
+        float g;
+        float b;
+    };
   
-  struct {
-    Vec2 xy;
-    float _dummy0;
-  };
+    struct {
+        Vec2 xy;
+        float _dummy0;
+    };
   
-  struct {
-    float _dummy1;
-    Vec2 yz;
-  };
+    struct {
+        float _dummy1;
+        Vec2 yz;
+    };
 };
 
 union Vec4 {
-  float elements[4];
+    float elements[4];
   
-  struct {
-    float x;
-    float y;
-    float z;
-    float w;
-  };
+    struct {
+        float x;
+        float y;
+        float z;
+        float w;
+    };
   
-  struct {
-    float r;
-    float g;
-    float b;
-    float a;
-  };
+    struct {
+        float r;
+        float g;
+        float b;
+        float a;
+    };
   
-  struct {
-    Vec3 xyz;
-    float _dummy0;
-  };
+    struct {
+        Vec3 xyz;
+        float _dummy0;
+    };
   
-  struct {
-    float _dummy1;
-    Vec3 yzw;
-  };
+    struct {
+        float _dummy1;
+        Vec3 yzw;
+    };
   
-  struct {
-    Vec2 xy;
-    float _dummy2;
-    float _dummy3;
-  };
+    struct {
+        Vec2 xy;
+        float _dummy2;
+        float _dummy3;
+    };
   
-  struct {
-    float _dummy4;
-    Vec2 yz;
-    float _dummy5;
-  };
+    struct {
+        float _dummy4;
+        Vec2 yz;
+        float _dummy5;
+    };
   
-  struct {
-    float _dummy6;
-    float _dummy7;
-    Vec2 zw;
-  };
+    struct {
+        float _dummy6;
+        float _dummy7;
+        Vec2 zw;
+    };
 };
 
 union Mat4 {
-  float elements[4 * 4];
+    float elements[4 * 4];
 };
 
 Mat4 create_mat4(float num);
@@ -165,13 +165,15 @@ float clamp(float v, float lower, float higher);
 float lerp(float lower, float higher, float v);
 float ilerp(float lower, float higher, float v);
 
+#ifndef NDEBUG
 void _gm_assert(int cond, const char *msg, int line, const char *file);
 void _gm_print_mat4(Mat4 *matrix);
 void _gm_print_vec2(Vec2 *vec);
 void _gm_print_vec3(Vec3 *vec);
 void _gm_print_vec4(Vec4 *vec);
+#endif // NDEBUG
 
-#endif // BEELINE_ALGEBRA_H_
+#endif // BEELINE_ALGEBRA_H
 #ifdef BEELINE_IMPLEMENTATION
 
 // ===========================================================================
@@ -179,181 +181,181 @@ void _gm_print_vec4(Vec4 *vec);
 // ===========================================================================
 Mat4 create_mat4(float num)
 {
-  Mat4 out = {};
+    Mat4 out = {};
   
-  for (int i = 0; i < 4 * 4; ++i) {
-    out.elements[i] = num;
-  }
+    for (int i = 0; i < 4 * 4; ++i) {
+        out.elements[i] = num;
+    }
   
-  return out;
+    return out;
 }
 
 Mat4 create_mat4()
 {
-  return create_mat4(0.0f);
+    return create_mat4(0.0f);
 }
 
 Mat4 create_mat4_identity(float num)
 {
-  Mat4 out = {};
+    Mat4 out = {};
   
-  out.elements[0] = num;
-  out.elements[5] = num;
-  out.elements[10] = num;
-  out.elements[15] = num;
+    out.elements[0] = num;
+    out.elements[5] = num;
+    out.elements[10] = num;
+    out.elements[15] = num;
   
-  return out;
+    return out;
 }
 
 Mat4 create_mat4_identity()
 {
-  return create_mat4_identity(1.0f);
+    return create_mat4_identity(1.0f);
 }
 
 Mat4 create_mat4_ortho(float l, float r, float b, float t, float n, float f)
 {
-  Mat4 out = {};
+    Mat4 out = {};
   
-  out.elements[0] = 2.0f / (r - l);
-  out.elements[3] = -(r + l) / (r - l);
-  out.elements[5] = 2.0f / (t - b);
-  out.elements[7] = -(t + b) / (t - b);
-  out.elements[10] = -2.0f / (f - n);
-  out.elements[11] = -(f + n) / (f - n);
-  out.elements[15] = 1.0f;
+    out.elements[0] = 2.0f / (r - l);
+    out.elements[3] = -(r + l) / (r - l);
+    out.elements[5] = 2.0f / (t - b);
+    out.elements[7] = -(t + b) / (t - b);
+    out.elements[10] = -2.0f / (f - n);
+    out.elements[11] = -(f + n) / (f - n);
+    out.elements[15] = 1.0f;
   
-  return out;
+    return out;
 }
 
 Mat4 create_mat4_ortho(float l, float r, float b, float t)
 {
-  Mat4 out = {};
+    Mat4 out = {};
   
-  out.elements[0] = 2.0f / (r - l);
-  out.elements[5] = 2.0f / (t - b);
-  out.elements[10] = -1.0f;
-  out.elements[15] = 1.0f;
-  out.elements[3] = -(r + l) / (r - l);
-  out.elements[7] = -(t + b) / (t - b);
+    out.elements[0] = 2.0f / (r - l);
+    out.elements[5] = 2.0f / (t - b);
+    out.elements[10] = -1.0f;
+    out.elements[15] = 1.0f;
+    out.elements[3] = -(r + l) / (r - l);
+    out.elements[7] = -(t + b) / (t - b);
   
-  return out;
+    return out;
 }
 
 Mat4 create_mat4_perspective(float fov_degrees, float aspect, float n, float f)
 {
-  Mat4 out = {};
+    Mat4 out = {};
   
-  float radians = fov_degrees * (3.14159f / 180.0f);
-  float t = n * tanf(radians / 2.0f);
-  float b = -t;
-  float r = t * aspect;
-  float l = -r;
+    float radians = fov_degrees * (3.14159f / 180.0f);
+    float t = n * tanf(radians / 2.0f);
+    float b = -t;
+    float r = t * aspect;
+    float l = -r;
   
-  out.elements[0] = 2.0f * n / (r - l);
-  out.elements[3] = -n * (r + l) / (r - l);
-  out.elements[5] = 2.0f * n / (t - b);
-  out.elements[7] = n * (t + b) / (t - b);
-  out.elements[10] = -(f + n) / (f - n);
-  out.elements[11] = 2.0f * f * n / (n - f);
-  out.elements[14] = -1.0f;
+    out.elements[0] = 2.0f * n / (r - l);
+    out.elements[3] = -n * (r + l) / (r - l);
+    out.elements[5] = 2.0f * n / (t - b);
+    out.elements[7] = n * (t + b) / (t - b);
+    out.elements[10] = -(f + n) / (f - n);
+    out.elements[11] = 2.0f * f * n / (n - f);
+    out.elements[14] = -1.0f;
   
-  return out;
+    return out;
 }
 
 void scale_all_mat4(Mat4 *matrix, float scale)
 {
-  for (int i = 0; i < 4 * 4; ++i) {
-    matrix->elements[i] *= scale;
-  }
+    for (int i = 0; i < 4 * 4; ++i) {
+        matrix->elements[i] *= scale;
+    }
 }
 
 void scale_assign_mat4(Mat4 *matrix, float x, float y, float z)
 {
-  matrix->elements[0] = x;
-  matrix->elements[5] = y;
-  matrix->elements[10] = z;
+    matrix->elements[0] = x;
+    matrix->elements[5] = y;
+    matrix->elements[10] = z;
 }
 
 void scale_mat4(Mat4 *matrix, float x, float y, float z)
 {
-  matrix->elements[0] *= x;
-  matrix->elements[5] *= y;
-  matrix->elements[10] *= z;
+    matrix->elements[0] *= x;
+    matrix->elements[5] *= y;
+    matrix->elements[10] *= z;
 }
 
 Mat4 sub_mat4(Mat4 *a, Mat4 *b)
 {
-  Mat4 out = {};
+    Mat4 out = {};
   
-  for (int i = 0; i < 4 * 4; ++i) {
-    out.elements[i] = a->elements[i] - b->elements[i];
-  }
+    for (int i = 0; i < 4 * 4; ++i) {
+        out.elements[i] = a->elements[i] - b->elements[i];
+    }
   
-  return out;
+    return out;
 }
 
 Mat4 add_mat4(Mat4 *a, Mat4 *b)
 {
-  Mat4 out = {};
+    Mat4 out = {};
   
-  for (int i = 0; i < 4 * 4; ++i) {
-    out.elements[i] = a->elements[i] + b->elements[i];
-  }
+    for (int i = 0; i < 4 * 4; ++i) {
+        out.elements[i] = a->elements[i] + b->elements[i];
+    }
   
-  return out;    
+    return out;    
 }
 
 Mat4 mult_mat4(Mat4 *a, Mat4 *b)
 {
-  Mat4 out = {};
+    Mat4 out = {};
   
-  for (int i = 0; i < 4; ++i) {
-    for (int j = 0; j < 4; ++j) {
-      for (int k = 0; k < 4; ++k) {
-        out.elements[i * 4 + j] += a->elements[i * 4 + k] * b->elements[k * 4 + j];
-      }
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            for (int k = 0; k < 4; ++k) {
+                out.elements[i * 4 + j] += a->elements[i * 4 + k] * b->elements[k * 4 + j];
+            }
+        }
     }
-  }
   
-  return out;
+    return out;
 }
 
 Mat4 transpose_mat4(Mat4 *matrix)
 {
-  Mat4 out = {};
+    Mat4 out = {};
   
-  for (int i = 0; i < 4; ++i) {
-    for (int j = 0; j < 4; ++j) {
-      out.elements[j * 4 + i] = matrix->elements[i * 4 + j];
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            out.elements[j * 4 + i] = matrix->elements[i * 4 + j];
+        }
     }
-  }
   
-  return out;
+    return out;
 }
 
 Mat4 create_mat4_translation(float x, float y, float z)
 {
-  Mat4 out = create_mat4_identity();
+    Mat4 out = create_mat4_identity();
   
-  out.elements[3] = x;
-  out.elements[7] = y;
-  out.elements[11] = z;
+    out.elements[3] = x;
+    out.elements[7] = y;
+    out.elements[11] = z;
   
-  return out;
+    return out;
 }
 
 void translate_add_mat4(Mat4 *matrix, float x, float y, float z)
 {
-  matrix->elements[3] += x;
-  matrix->elements[7] += y;
-  matrix->elements[11] += z;
+    matrix->elements[3] += x;
+    matrix->elements[7] += y;
+    matrix->elements[11] += z;
 }
 
 void translate_mat4(Mat4 *matrix, float x, float y, float z)
 {
-  matrix->elements[3] = x;
-  matrix->elements[7] = y;
-  matrix->elements[11] = z;
+    matrix->elements[3] = x;
+    matrix->elements[7] = y;
+    matrix->elements[11] = z;
 }
 
 // ===========================================================================
@@ -361,262 +363,262 @@ void translate_mat4(Mat4 *matrix, float x, float y, float z)
 // ===========================================================================
 Vec2 create_vec2(float x, float y)
 {
-  Vec2 out = {};
+    Vec2 out = {};
   
-  out.x = x;
-  out.y = y;
+    out.x = x;
+    out.y = y;
   
-  return out;
+    return out;
 }
 
 Vec2 create_vec2()
 {
-  return create_vec2(0.0f, 0.0f);
+    return create_vec2(0.0f, 0.0f);
 }
 
 void scale_vec2(Vec2 *vec, float scale)
 {
-  vec->x *= scale;
-  vec->y *= scale;
+    vec->x *= scale;
+    vec->y *= scale;
 }
 
 float dot_vec2(Vec2 *a, Vec2 *b)
 {
-  return (a->x * b->x) + (a->y * b->y);
+    return (a->x * b->x) + (a->y * b->y);
 }
 
 void add_vec2_scalar(Vec2 *vec, float scalar)
 {
-  vec->x += scalar;
-  vec->y += scalar;
+    vec->x += scalar;
+    vec->y += scalar;
 }
 
 void negate_vec2(Vec2 *vec)
 {
-  vec->x = -(vec->x);
-  vec->y = -(vec->y);
+    vec->x = -(vec->x);
+    vec->y = -(vec->y);
 }
 
 Vec2 add_vec2(Vec2 *a, Vec2 *b)
 {
-  Vec2 out = {};
+    Vec2 out = {};
   
-  out.x = a->x + b->x;
-  out.y = a->y + b->y;
+    out.x = a->x + b->x;
+    out.y = a->y + b->y;
   
-  return out;
+    return out;
 }
 
 Vec2 sub_vec2(Vec2 *a, Vec2 *b)
 {
-  Vec2 out = {};
+    Vec2 out = {};
   
-  out.x = a->x - b->x;
-  out.y = a->y - b->y;
+    out.x = a->x - b->x;
+    out.y = a->y - b->y;
   
-  return out;
+    return out;
 }
 
 float len_vec2(Vec2 *vec)
 {
-  return (vec->x * vec->x) + (vec->y * vec->y);
+    return (vec->x * vec->x) + (vec->y * vec->y);
 }
 
 float len_vec2_sqrt(Vec2 *vec)
 {
-  return sqrtf(len_vec2(vec));
+    return sqrtf(len_vec2(vec));
 }
 
 void norm_vec2(Vec2 *vec)
 {
-  float len = len_vec2_sqrt(vec);
+    float len = len_vec2_sqrt(vec);
   
-  vec->x = vec->x / len;
-  vec->y = vec->y / len;
+    vec->x = vec->x / len;
+    vec->y = vec->y / len;
 }
 
 Vec3 create_vec3(float x, float y, float z)
 {
-  Vec3 out = {};
+    Vec3 out = {};
   
-  out.x = x;
-  out.y = y;
-  out.z = z;
+    out.x = x;
+    out.y = y;
+    out.z = z;
   
-  return out;
+    return out;
 }
 
 Vec3 create_vec3()
 {
-  return create_vec3(0.0f, 0.0f, 0.0f);
+    return create_vec3(0.0f, 0.0f, 0.0f);
 }
 
 void scale_vec3(Vec3 *vec, float scale)
 {
-  vec->x *= scale;
-  vec->y *= scale;
-  vec->z *= scale;
+    vec->x *= scale;
+    vec->y *= scale;
+    vec->z *= scale;
 }
 
 float dot_vec3(Vec3 *a, Vec3 *b)
 {
-  return (a->x * b->x) + (a->y * b->y) + (a->z * b->z);
+    return (a->x * b->x) + (a->y * b->y) + (a->z * b->z);
 }
 
 Vec3 cross_vec3(Vec3 *a, Vec3 *b)
 {
-  Vec3 out = {};
+    Vec3 out = {};
   
-  out.x = (a->y * b->z) - (a->z * b->y);
-  out.y = (a->z * b->x) - (a->x * b->z);
-  out.z = (a->x * b->y) - (a->y * b->x);
+    out.x = (a->y * b->z) - (a->z * b->y);
+    out.y = (a->z * b->x) - (a->x * b->z);
+    out.z = (a->x * b->y) - (a->y * b->x);
   
-  return out;
+    return out;
 }
 
 void add_vec3_scalar(Vec3 *vec, float scalar)
 {
-  vec->x += scalar;
-  vec->y += scalar;
-  vec->z += scalar;
+    vec->x += scalar;
+    vec->y += scalar;
+    vec->z += scalar;
 }
 
 void negate_vec3(Vec3 *vec)
 {
-  vec->x = -(vec->x);
-  vec->y = -(vec->y);
-  vec->z = -(vec->z); 
+    vec->x = -(vec->x);
+    vec->y = -(vec->y);
+    vec->z = -(vec->z); 
 }
 
 Vec3 add_vec3(Vec3 *a, Vec3 *b)
 {
-  Vec3 out = {};
+    Vec3 out = {};
   
-  out.x = a->x + b->x;
-  out.y = a->y + b->y;
-  out.z = a->z + b->z;
+    out.x = a->x + b->x;
+    out.y = a->y + b->y;
+    out.z = a->z + b->z;
   
-  return out;
+    return out;
 }
 
 Vec3 sub_vec3(Vec3 *a, Vec3 *b)
 {
-  Vec3 out = {};
+    Vec3 out = {};
   
-  out.x = a->x - b->x;
-  out.y = a->y - b->y;
-  out.z = a->z - b->z;
+    out.x = a->x - b->x;
+    out.y = a->y - b->y;
+    out.z = a->z - b->z;
   
-  return out;
+    return out;
 }
 
 float len_vec3(Vec3 *vec)
 {
-  return (vec->x * vec->x) + (vec->y * vec->y) + (vec->z * vec->z);
+    return (vec->x * vec->x) + (vec->y * vec->y) + (vec->z * vec->z);
 }
 
 float len_vec3_sqrt(Vec3 *vec)
 {
-  return sqrtf(len_vec3(vec));
+    return sqrtf(len_vec3(vec));
 }
 
 void norm_vec3(Vec3 *vec)
 {
-  float len = len_vec3_sqrt(vec);
+    float len = len_vec3_sqrt(vec);
   
-  vec->x = vec->x / len;
-  vec->y = vec->y / len;
-  vec->z = vec->z / len;
+    vec->x = vec->x / len;
+    vec->y = vec->y / len;
+    vec->z = vec->z / len;
 }
 
 Vec4 create_vec4(float x, float y, float z, float w)
 {
-  Vec4 out = {};
+    Vec4 out = {};
   
-  out.x = x;
-  out.y = y;
-  out.z = z;
-  out.w = w;
+    out.x = x;
+    out.y = y;
+    out.z = z;
+    out.w = w;
   
-  return out;
+    return out;
 }
 
 Vec4 create_vec4()
 {
-  return create_vec4(0.0f, 0.0f, 0.0f, 0.0f);
+    return create_vec4(0.0f, 0.0f, 0.0f, 0.0f);
 }
 
 void scale_vec4(Vec4 *vec, float scale)
 {
-  vec->x *= scale;
-  vec->y *= scale;
-  vec->z *= scale;
-  vec->w *= scale;
+    vec->x *= scale;
+    vec->y *= scale;
+    vec->z *= scale;
+    vec->w *= scale;
 }
 
 float dot_vec4(Vec4 *a, Vec4 *b)
 {
-  return (a->x * b->x) + (a->y * b->y) + (a->z * b->z) + (a->w * b->w);
+    return (a->x * b->x) + (a->y * b->y) + (a->z * b->z) + (a->w * b->w);
 }
 
 void add_vec4_scalar(Vec4 *vec, float scalar)
 {
-  vec->x += scalar;
-  vec->y += scalar;
-  vec->z += scalar;
-  vec->w += scalar;
+    vec->x += scalar;
+    vec->y += scalar;
+    vec->z += scalar;
+    vec->w += scalar;
 }
 
 void negate_vec4(Vec4 *vec)
 {
-  vec->x = -(vec->x);
-  vec->y = -(vec->y);
-  vec->z = -(vec->z);
-  vec->w = -(vec->w);
+    vec->x = -(vec->x);
+    vec->y = -(vec->y);
+    vec->z = -(vec->z);
+    vec->w = -(vec->w);
 }
 
 Vec4 add_vec4(Vec4 *a, Vec4 *b)
 {
-  Vec4 out = {};
+    Vec4 out = {};
   
-  out.x = a->x + b->x;
-  out.y = a->y + b->y;
-  out.z = a->z + b->z;
-  out.w = a->w + b->w;
+    out.x = a->x + b->x;
+    out.y = a->y + b->y;
+    out.z = a->z + b->z;
+    out.w = a->w + b->w;
   
-  return out;   
+    return out;   
 }
 
 Vec4 sub_vec4(Vec4 *a, Vec4 *b)
 {
-  Vec4 out = {};
+    Vec4 out = {};
   
-  out.x = a->x - b->x;
-  out.y = a->y - b->y;
-  out.z = a->z - b->z;
-  out.w = a->w - b->w;
+    out.x = a->x - b->x;
+    out.y = a->y - b->y;
+    out.z = a->z - b->z;
+    out.w = a->w - b->w;
   
-  return out;   
+    return out;   
 }
 
 float len_vec4(Vec4 *vec)
 {
-  return (vec->x * vec->x) + (vec->y * vec->y) + (vec->z * vec->z) + (vec->w * vec->w);
+    return (vec->x * vec->x) + (vec->y * vec->y) + (vec->z * vec->z) + (vec->w * vec->w);
 }
 
 float len_vec4_sqrt(Vec4 *vec)
 {
-  return sqrtf(len_vec4(vec));
+    return sqrtf(len_vec4(vec));
 }
 
 void norm_vec4(Vec4 *vec)
 {
-  float len = len_vec4_sqrt(vec);
+    float len = len_vec4_sqrt(vec);
   
-  vec->x = vec->x / len;
-  vec->y = vec->y / len;
-  vec->z = vec->z / len;
-  vec->w = vec->w / len;
+    vec->x = vec->x / len;
+    vec->y = vec->y / len;
+    vec->z = vec->z / len;
+    vec->w = vec->w / len;
 }
 
 // ===========================================================================
@@ -624,66 +626,67 @@ void norm_vec4(Vec4 *vec)
 // ===========================================================================
 float min(float a, float b)
 {
-  return (a > b) ? b : a;
+    return (a > b) ? b : a;
 }
 
 float max(float a, float b) 
 {
-  return (a > b) ? a : b;
+    return (a > b) ? a : b;
 }
 
 float clamp(float v, float lower, float higher)
 {
-  return max(lower, min(v, higher));
+    return max(lower, min(v, higher));
 }
 
 float lerp(float lower, float higher, float v)
 {
-  return (higher - lower) * v + lower;
+    return (higher - lower) * v + lower;
 }
 
 float ilerp(float lower, float higher, float v)
 {
-  return (v - lower) / (higher - lower);
+    return (v - lower) / (higher - lower);
 }
 
 // ===========================================================================
-// Utilities
+// Debug utilities
 // ===========================================================================
+#ifndef NDEBUG
 void _gm_assert(int cond, const char *msg, int line, const char *file)
 {
-  if (!cond) {
-    printf("Assertion failed in %s(%d)\n\t%s\n", file, line, msg);
-    abort();
-  }
+    if (!cond) {
+        printf("Assertion failed in %s(%d)\n\t%s\n", file, line, msg);
+        abort();
+    }
 }
 
 void _gm_print_mat4(Mat4 *matrix)
 {
-  printf("\n");
-  
-  for (int i = 0; i < 4; ++i) {
-    for (int j = 0; j < 4; ++j) {
-      printf("%f ", matrix->elements[i * 4 + j]);
-    }
-    
     printf("\n");
-  }
+  
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            printf("%f ", matrix->elements[i * 4 + j]);
+        }
+    
+        printf("\n");
+    }
 }
 
 void _gm_print_vec2(Vec2 *vec)
 {
-  printf("x: %.2f y: %.2f\n", vec->x, vec->y);
+    printf("x: %.2f y: %.2f\n", vec->x, vec->y);
 }
 
 void _gm_print_vec3(Vec3 *vec)
 {
-  printf("x: %.2f y: %.2f z: %.2f\n", vec->x, vec->y, vec->z);
+    printf("x: %.2f y: %.2f z: %.2f\n", vec->x, vec->y, vec->z);
 }
 
 void _gm_print_vec4(Vec4 *vec)
 {
-  printf("x: %.2f y: %.2f z: %.2f w: %.2f\n", vec->x, vec->y, vec->z, vec->w);
+    printf("x: %.2f y: %.2f z: %.2f w: %.2f\n", vec->x, vec->y, vec->z, vec->w);
 }
-
+#endif // NDEBUG
 #endif // BEELINE_IMPLEMENTATION
