1f7d4c3df9e58bbf70c857b1813d75f6c937484d
Aiden <marcin.madry@interia.com>
2022-11-25 18:53:44 +0100

    Add flag for better error parsing

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
 
 REM Change this to your visual studio's 'vcvars64.bat' script path
-set MSVC_PATH="YOUR_PATH"
-set CXXFLAGS=/std:c++17 /EHsc /W4 /WX /wd4996 /wd4201 /nologo %*
+set MSVC_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build"
+set CXXFLAGS=/std:c++17 /EHsc /W4 /WX /FC /wd4996 /wd4201 /nologo %*
 
 call %MSVC_PATH%\vcvars64.bat
 
